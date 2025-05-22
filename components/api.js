e5a23e91fd3072e5b91e732b923b7cde86ea21d7
Casey Overton <68711930+CaseyOverton@users.noreply.github.com>
2025-05-22 18:34:06 -0400

    Added functioning api

--- components/api.js
+++ components/api.js
@@ -1,27 +1,57 @@
-import {Task} from '@lit/task';
+import { LitElement, html, css } from 'lit';
 
-class MyApi extends LitElement {
-
-    _getDataTask = new Task(
-      this,
-      {
-        task: async () => {
-          const response = await fetch(`https://kitsu.io/api/edge/anime`);
-          return response.json();
-        },
-        args: () => []
+export class MyApi extends LitElement {
+    static properties = {
+      data: { type: Object }
+    };
+  
+    constructor() {
+      super();
+      this.data = {};
+    }
+  
+    // fetch user data from API using native fetch
+    async getProfile() {
+      try {
+        const response = await fetch('https://kitsu.io/api/edge/anime?page[limit]=10');
+        const result = await response.json();
+        this.data = result;
+        this.requestUpdate();
+      } catch (err) {
+        console.error('Error fetching profile:', err);
       }
-    );
+    }
+  
+    connectedCallback() {
+      super.connectedCallback();
+      this.getProfile();
+    }
   
     render() {
       return html`
-        <button @click=${this._onClick}>Get Data</button>
-        <p>Get Data</p>
+        <div>
+          <h2>Anime List</h2>
+          <ul>
+            ${Array.isArray(this.data.data)
+              ? this.data.data.map(
+                  item => html`<li>${item.attributes.canonicalTitle}</li>`
+                )
+              : html`<li>Loading…</li>`}
+          </ul>
+        </div>
       `;
     }
   
-    _onClick() {
-      this._getDataTask.run();
-    }
+    static styles = css`
+      :host {
+        display: block;
+        font-family: Arial, sans-serif;
+      }
+  
+      h2 {
+        color: #333;
+      }
+    `;
   }
+  
 customElements.define('my-api', MyApi);
